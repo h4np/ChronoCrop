2b208a02540c80e0d2aa7d8426ca312ca6e51c8b
Hannu <varahahmo1@gmail.com>
2023-11-21 20:59:01 +0200

    tabs, split into functions

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module chronocut/init
 
-go 1.21.1
+go 1.21
 
 require fyne.io/fyne/v2 v2.4.0
 
--- main.go
+++ main.go
@@ -7,37 +7,57 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-    app := app.New()
-    window := app.NewWindow("Chronocut")
+	app := app.New()
+	window := app.NewWindow("Chronocut")
 
-    fileNameLabel := widget.NewLabel("Selected file: ")
+	window.SetContent(renderUI(app, window))
+	window.Resize(fyne.NewSize(600, 600))
+	window.ShowAndRun()
+}
 
-    selectButton := widget.NewButton("Select file", func() {
-        dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
-            if err != nil {
-                fmt.Println(err)
-                return
-            }
-            fileNameLabel.SetText("Selected file: " + reader.URI().Path())
-        }, window).Show()
+func renderUI(app fyne.App, window fyne.Window) *fyne.Container {
+	tabs := topTabs()
 
-    })
+	fileNameLabel := widget.NewLabel("Selected file: ")
 
+	selectButton := widget.NewButton("Select file", func() {
+		dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			if reader != nil {
+				fileNameLabel.SetText("Selected file: " + reader.URI().Path())
+				fmt.Println("reader nil")
+				reader.Close()
+			}
+			fileNameLabel.SetText("Selected file: " + reader.URI().Path())
+		}, window).Show()
+	})
+	quitButton := widget.NewButton("Quit", func() {
+		app.Quit()
+	})
 
-    content := container.NewVBox(
-        widget.NewLabel("Chronocut"),
-        fileNameLabel,
-        selectButton,
-        widget.NewButton("Quit", func() {
-            app.Quit()
-        }),
-    )
+	UI := container.NewVBox(
+		tabs,
+		fileNameLabel,
+		selectButton,
+		quitButton,
+	)
 
-    window.SetContent(content)
-    window.Resize(fyne.NewSize(600, 600))
-    window.ShowAndRun()
+	return UI
+}
+
+func topTabs() *container.AppTabs {
+	tabs := container.NewAppTabs(
+		container.NewTabItemWithIcon("Video", theme.FileVideoIcon(), widget.NewLabel("")),
+		container.NewTabItemWithIcon("Download", theme.DownloadIcon(), widget.NewLabel("")),
+		container.NewTabItemWithIcon("Settings", theme.SettingsIcon(), widget.NewLabel("")),
+	)
+	return tabs
 }
